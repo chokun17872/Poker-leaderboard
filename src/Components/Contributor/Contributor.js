0cdd4d2de167a3726ce1722795244b4ae77e16d0
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-03-22 15:07:10 +0700

    style: change css to scope at specific component

--- src/Components/Contributor/Contributor.js
+++ src/Components/Contributor/Contributor.js
@@ -5,13 +5,19 @@ import "./Contributor.css";
 class Contributor extends Component {
   render() {
     return (
-      <tr>
-        <th scope="row">{this.props.info.rank}</th>
+      <tr className="Contributor">
+        <th className="rank" scope="row">
+          {this.props.info.rank}
+        </th>
         <td>
-          <img src={profileImg} alt={this.props.info.name} />
+          <img
+            className="profile"
+            src={profileImg}
+            alt={this.props.info.name}
+          />
         </td>
-        <td>{this.props.info.name}</td>
-        <td>{this.props.info.point}</td>
+        <td className="name">{this.props.info.name}</td>
+        <td className="point">{this.props.info.point}</td>
       </tr>
     );
   }
