6677597b63f3a5c22d22e77b532a2c6f837178a0
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-05 19:20:42 +0700

    feat: add title

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Leaderboard from "../Components/Leaderboard/Leaderboard";
+import LeaderboardName from "../Components/LeaderboardName/LeaderboardName";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <LeaderboardName />
         <Leaderboard />
       </div>
     );
---
+++ src/Components/LeaderboardName/LeaderboardName.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LeaderboardName extends Component {
+  render() {
+    return (
+      <div className="LeaderboardName">
+        <p className="title">Contributor Ranking</p>
+        <p className="facebookGroup">Poker Thailand</p>
+      </div>
+    );
+  }
+}
+
+export default LeaderboardName;
