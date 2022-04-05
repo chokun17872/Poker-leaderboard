b38e1255e9292053ff67c19ce41f4f2e27409a12
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-05 19:46:03 +0700

    feat: add href link to facebook group

--- src/Components/LeaderboardTitle/LeaderboardTitle.js
+++ src/Components/LeaderboardTitle/LeaderboardTitle.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
 import "./LeaderboardTitle.css";
+import fbLogo from "./fbLogo.png";
+// import fbLogo2 from "./fbLogo2.png";
 
 class LeaderboardTitle extends Component {
   render() {
     return (
       <div className="LeaderboardTitle">
         <p className="title">Contributor Ranking</p>
-        <p className="facebookGroup">Poker Thailand</p>
+        <div className="facebookGroup">
+          <p className="groupName">Poker Thailand</p>
+          <a
+            className="groupLink"
+            href="https://www.facebook.com/groups/223422232142743/"
+          >
+            <img className="logo" src={fbLogo} alt="fbLogo"></img>
+          </a>
+        </div>
       </div>
     );
   }
