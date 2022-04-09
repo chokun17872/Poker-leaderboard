75331fc3b9261a6db3b2b7f2e5153ea9a6e09d57
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-09 10:58:05 +0700

    feat: change className to facebookGroup only

--- src/Components/LeaderboardTitle/LeaderboardTitle.js
+++ src/Components/LeaderboardTitle/LeaderboardTitle.js
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import "./LeaderboardTitle.css";
 import fbLogo from "./fbLogo.png";
-// import fbLogo2 from "./fbLogo2.png";
 
 class LeaderboardTitle extends Component {
   render() {
     return (
       <div className="LeaderboardTitle">
         <p className="title">Contributor Ranking</p>
-        <div className="facebookGroup">
-          <p className="groupName">Poker Thailand</p>
+        <p className="facebookGroup">
+          Poker Thailand Community
           <a
             className="groupLink"
             href="https://www.facebook.com/groups/223422232142743/"
           >
             <img className="logo" src={fbLogo} alt="fbLogo"></img>
           </a>
-        </div>
+        </p>
       </div>
     );
   }
