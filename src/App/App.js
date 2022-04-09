d999fd29f2ec7d6068a14d08e9d3977ca408b6a6
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-09 11:15:50 +0700

    feat: move Reward component to App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Leaderboard from "../Components/Leaderboard/Leaderboard";
 import LeaderboardTitle from "../Components/LeaderboardTitle/LeaderboardTitle";
+import Reward from "../Components/Reward/Reward";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Reward />
         <LeaderboardTitle />
         <Leaderboard />
       </div>
