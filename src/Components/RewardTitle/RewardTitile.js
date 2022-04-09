c259c4dea417b36ebb73243638edb75e93b929f3
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-09 11:24:55 +0700

    init: RewardTitle.js

---
+++ src/Components/RewardTitle/RewardTitile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class RewardTitle extends Component {
+  render() {
+    return (
+      <div>
+        <p>ของรางวัล</p>
+        <p>วันที่ 31 กุมภาพันธ์ 2077</p>
+      </div>
+    );
+  }
+}
+
+export default RewardTitle;
