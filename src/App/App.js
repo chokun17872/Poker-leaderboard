71ca6a5d0534cc07aabdaa6d75487d8811b248a3
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-09 11:36:59 +0700

    feat: replace RewardTitle with Reward in App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Leaderboard from "../Components/Leaderboard/Leaderboard";
 import LeaderboardTitle from "../Components/LeaderboardTitle/LeaderboardTitle";
-import RewardTitle from "../Components/RewardTitle/RewardTitle";
+import Reward from "../Components/Reward/Reward";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <RewardTitle />
+        <Reward />
         <LeaderboardTitle />
         <Leaderboard />
       </div>
