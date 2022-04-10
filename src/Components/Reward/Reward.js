b251c27409d8dcf8c630298b988d8a8b80cb0729
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-10 18:31:38 +0700

    feat: move modal to Reward.js use react-bootstrap

--- src/Components/Reward/Reward.js
+++ src/Components/Reward/Reward.js
@@ -1,24 +1,31 @@
-import React, { Component } from "react";
-import "./Reward.css";
-import Modal from "../Modal/Modal";
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
-class Reward extends Component {
-  render() {
-    return (
-      <div className="Reward">
-        <button
-          type="button"
-          className="btn btn-dark"
-          data-toggle="modal"
-          data-target="#reward"
-        >
-          ของรางวัล
-        </button>
+function RewardModal() {
+  const [show, setShow] = useState(false);
 
-        <Modal />
-      </div>
-    );
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        ของรางวัล
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body></Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
-export default Reward;
+export default RewardModal;
