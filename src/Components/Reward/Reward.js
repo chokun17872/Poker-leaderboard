16b822815b2f50342d759ef0fe96b7d29ae61a0b
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-10 11:11:12 +0700

    feat: move Modal to Reward.js

--- src/Components/Reward/Reward.js
+++ src/Components/Reward/Reward.js
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import "./Reward.css";
+import Modal from "../Modal/Modal";
 
 class Reward extends Component {
   render() {
     return (
-      <button
-        type="button"
-        className="reward btn btn-dark"
-        data-toggle="modal"
-        data-target="#reward"
-      >
-        ของรางวัล
-      </button>
+      <div className="Reward">
+        <button
+          type="button"
+          className="btn btn-dark"
+          data-toggle="modal"
+          data-target="#reward"
+        >
+          ของรางวัล
+        </button>
+
+        <Modal />
+      </div>
     );
   }
 }
