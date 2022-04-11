2621d6dc653741eafcd7a3d8c8eadaf741a64c88
Chavalvit Keartnattakorn <cho17872@hotmail.com>
2022-04-11 13:34:50 +0700

    feat: init RewardList.js

--- src/App/App.js
+++ src/App/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 import Leaderboard from "../Components/Leaderboard/Leaderboard";
 import LeaderboardTitle from "../Components/LeaderboardTitle/LeaderboardTitle";
 import RewardModal from "../Components/RewardModal/RewardModal";
-import Reward from "../Components/Reward/Reward";
+import RewardList from "../Components/RewardList/RewardList";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Reward />
+        <RewardList />
         <RewardModal />
         <LeaderboardTitle />
         <Leaderboard />
---
+++ src/Components/RewardList/RewardList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Reward from "../Reward/Reward";
+
+class RewardList extends Component {
+  render() {
+    return (
+      <div>
+        <Reward />
+        <Reward />
+        <Reward />
+      </div>
+    );
+  }
+}
+
+export default RewardList;
